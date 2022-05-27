8fa774e32b5568047c34bead6f85dfb997416a1b
arozek <a.rozek@ed.ac.uk>
2022-05-27 13:57:26 +0100

    Serializer issues resolved

--- inflammation/serializers.py
+++ inflammation/serializers.py
@@ -1,20 +1,60 @@
 from inflammation import models
+import json
 
 
-class PatientSerializer:
-    model = models.Patient
+class Serializer:
+    @classmethod
+    def serialize(cls, instances):
+        raise NotImplementedError
+
+    @classmethod
+    def save(cls, instances, path):
+        raise NotImplementedError
+
+    @classmethod
+    def deserialize(cls, data):
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, path):
+        raise NotImplementedError
+
+
+class ObservationSerializer(Serializer):
+    model = models.Observation
 
     @classmethod
     def serialize(cls, instances):
         return [{
-            'name': instance.name,
-            'observations': instance.observations,
+            'day': instance.day,
+            'value': instance.value,
         } for instance in instances]
 
     @classmethod
     def deserialize(cls, data):
         return [cls.model(**d) for d in data]
 
+
+
+class PatientSerializer(Serializer):
+    model = models.Patient
+
+    @classmethod
+    def serialize(cls, instances):
+        return [{
+            'name': instance.name,
+            'observations': ObservationSerializer.serialize(instance.observations),
+        } for instance in instances]
+
+    @classmethod
+    def deserialize(cls, data):
+        instances = []
+
+        for item in data:
+            item['observations'] = ObservationSerializer.deserialize(item.pop('observations'))
+            instances.append(cls.model(**item))
+
+        return instances
 
 class PatientJSONSerializer(PatientSerializer):
     @classmethod
